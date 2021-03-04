f7edaba36d8a26cfd3dc4f12b9da92cf742aa461
Piotr Falkowski <piotr.falkowski@hitachi-powergrids.com>
2021-03-04 15:45:55 +0100

    Changed to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Homepage from "./features/homepage";
 import Success from "./features/success";
